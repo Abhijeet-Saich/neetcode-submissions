720d965eef5e439a963939622476014b9d2976be
coder_of_cosmos <76621794+Abhijeet-Saich@users.noreply.github.com>
2026-05-03 00:57:40 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+
+        let memo = new Array(n+1).fill(-1);  //no of ways we can reach to top from 'kth' stair
+
+        // function recur(k){
+
+        //     if(k == n) return 1;   //reached at destination
+        //     if(k > n ) return 0;
+
+        //     if(memo[k] != -1) return memo[k];
+
+        //     let jump1 = recur(k + 1);
+        //     let jump2 = recur(k + 2);
+
+        //     memo[k] = jump1 + jump2;
+
+        //     return memo[k];
+        // }
+
+        function bottomUp(n){
+            memo[n] = 1;
+
+            for(let i = n-1; i >= 0; i--){
+                let jump1 = memo[i+1];
+                let jump2 = (i+2 <= n) ? memo[i+2] : 0;
+
+                memo[i] = jump1 + jump2;
+            }
+
+            return memo[0];
+        }
+
+        // return recur(0)
+        return bottomUp(n)
+    }
+}
