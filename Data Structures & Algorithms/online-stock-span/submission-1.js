48106d12ab1201545faf31dac0d1e8dab22785fb
coder_of_cosmos <76621794+Abhijeet-Saich@users.noreply.github.com>
2026-05-06 17:03:29 +0530

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.js
@@ -0,0 +1,32 @@
+class StockSpanner {
+    constructor() {
+        this.stocks = [];
+        this.maxStack = [];
+    }
+
+    /**
+     * @param {number} price
+     * @return {number}
+     */
+    next(price) {
+
+        let count = 1;
+        while(this.maxStack.length && price >= this.maxStack[this.maxStack.length - 1].val){
+            count += this.maxStack.pop().span;
+        }
+        
+        let obj = {
+            val : price,
+            span : count
+        }
+
+        this.maxStack.push(obj);
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
